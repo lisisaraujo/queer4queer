6431b40310c97f552d0c817e22a432f3d9b19b8a
Lisis Ruschel <laruschel@proton.me>
2023-12-23 16:38:27 +0100

    added mapbox file again but in git ignore

---
+++ mapbox.js
@@ -0,0 +1,6 @@
+import GeocoderService from "@mapbox/mapbox-sdk/services/geocoding";
+
+export const accessToken =
+  "pk.eyJ1IjoiZGFsYWxhbWFkIiwiYSI6ImNsZXk3OXIwNDBrcW8zcm4wNGx0c3JiZm0ifQ.JUyYd7DDfX7U3VxqY9b_KA";
+
+export const geocoder = GeocoderService({ accessToken });
