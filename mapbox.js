9d27c003bb0ace9dd7933b381a4c6b6e9d939589
Lisis Ruschel <laruschel@proton.me>
2023-12-24 16:55:06 +0100

    put mapbox file back in git ignore because mapbox access token doesnt work when retrieved from env file

--- mapbox.js
+++ mapbox.js
@@ -1,5 +1,11 @@
 import GeocoderService from "@mapbox/mapbox-sdk/services/geocoding";
 
-const MapboxAccessToken = process.env.MAPBOX_ACCESS_TOKEN;
+export const accessToken =
+  "pk.eyJ1IjoiZGFsYWxhbWFkIiwiYSI6ImNsZXk3OXIwNDBrcW8zcm4wNGx0c3JiZm0ifQ.JUyYd7DDfX7U3VxqY9b_KA";
+  
+export const MapboxAccessToken = process.env.MAPBOX_ACCESS_TOKEN;
 
-export const geocoder = GeocoderService({ MapboxAccessToken });
+export const geocoder = GeocoderService({ accessToken });
+
+
+
